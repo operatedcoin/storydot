54d6cea65167bae25c7fbe3c35926d5bae60ceb5
Nick <nck@operatedcoin.com>
2024-01-07 15:54:29 +1100

    ble working

--- screens/experiences/townHall/TownHallDashboardScreen.js
+++ screens/experiences/townHall/TownHallDashboardScreen.js
@@ -1,16 +1,53 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import BleDashboard from '../../../components/ble/BleDashboardComponent'; // Adjust the path as necessary
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { BleManager } from 'react-native-ble-plx';
 import { globalStyles } from '../../../themes/globalStyles'; // Import global styles
 
-
 const TownHallDashboardScreen = () => {
+  const [devices, setDevices] = useState([]);
+  const manager = new BleManager();
+
+  useEffect(() => {
+      const scanDevices = () => {
+          manager.startDeviceScan(null, null, (error, device) => {
+              if (device && (device.localName === 'green' || device.localName === 'Yellow')) {
+                  setDevices(prevDevices => [...prevDevices, { name: device.localName, rssi: device.rssi }]);
+              }
+          });
+
+          setTimeout(() => {
+              manager.stopDeviceScan();
+          }, 2000); // Stop scanning after 2 seconds
+      };
+
+      const interval = setInterval(scanDevices, 2000); // Scan every 2 seconds
+
+      return () => {
+          clearInterval(interval);
+          manager.destroy();
+      };
+  }, []);
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Town Hall Dashboard Screen</Text>
-      <BleDashboard />
+    <View style={styles.container}>
+        {devices.map((device, index) => (
+            <Text key={index} style={styles.deviceText}>{device.name}: RSSI {device.rssi}</Text>
+        ))}
     </View>
-  );
+);
 };
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1, // This ensures that the view takes up the full space
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  deviceText: {
+      // Any additional styling for text goes here
+  }
+});
+
+
 
 export default TownHallDashboardScreen;
